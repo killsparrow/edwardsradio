54d0105adaaa35f0e2b20e8e00a6bd90fad907be
Evan Parker <parkerevan@icloud.com>
2026-03-18 11:08:31 -0400

    text update

--- app/underwater/page.tsx
+++ app/underwater/page.tsx
@@ -43,8 +43,6 @@ export default function Underwater() {
 
         <div className="mt-8 max-w-2xl p-4 mx-auto text-center mb-8">
           <h1 className="text-[#bfb689]" >Underwater</h1>
-
-          <p className="mb-4 text-white">This album was recorded using analog principles, honest full takes, and no fancy fixins.</p>
   
           <p className="mb-4 text-white">All songs were written and recorded by Edwards Radio.<br /> Home recorded demos 2026&copy; .</p>
 
