afe3a63ce29a73d59302da3d09f20a5069b0e150
Evan Parker <cube@EvansMacStudio.lan>
2026-05-24 14:06:32 -0400

    mu

--- app/wilderness/page.tsx
+++ app/wilderness/page.tsx
@@ -18,8 +18,8 @@ type WildernessTrack = {
 const TRACKS: WildernessTrack[] = [
   { key: "Love Song", display: "Love Song", time: "02:56" },
   { key: "Rescue for the Heartache", display: "Rescue for the Heartache", time: "03:54" },
-  { key: "Wilderness", display: "Wilderness", time: "03:37" },
   { key: "Breaking Part", display: "The Breaking Part", time: "03:55" },
+  { key: "Wilderness", display: "Wilderness", time: "03:37" },
   { key: "Batten the Hatches", display: "Batten the Hatches", time: "03:11" },
   { key: "No One", display: "No One", time: "05:10" },
   { key: "Warpath", display: "Warpath", time: "04:30" },
